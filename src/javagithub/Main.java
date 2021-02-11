14975ba9b1a7a5fecad831c6f6af713726aa708f
Joao <Joao@ASROCK-J5005>
2021-02-11 12:27:34 +0000

    Primeira versão em teste.

---
+++ src/javagithub/Main.java
@@ -0,0 +1,12 @@
+package javagithub;
+
+public class Main {
+
+    // Demonstração de utilização de um repositório com
+    // o GitHub. 
+    
+    public static void main(String[] args) {
+        System.out.println("Olá GitHub!");
+    }
+    
+}
